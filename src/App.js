20c12d2113104adfc8da8c9cad3531fe39580a98
chrisclarkedev <ceecee@CeeCees-MacBook-Air.local>
2019-10-31 11:35:43 -0400

    added media queries to images

--- src/App.js
+++ src/App.js
@@ -6,17 +6,23 @@ function App() {
     <div className="App">
       <h1>Testing</h1>
       <ul>
-        <li><img src="https://images.unsplash.com/photo-1491001224967-2c0ee18a7c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
+        <li><img src="https://images.unsplash.com/photo-1491001224967-2c0ee18a7c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9"></img>
+        </li>
+        <li><img src="https://images.unsplash.com/photo-1484353371297-d8cfd2895020?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1057&q=80"></img>
         </li>
         <li><img src="https://images.unsplash.com/photo-1503179579247-e6946936f17b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
         </li>
-        <li><img src="https://images.unsplash.com/photo-1484371924254-a44002341ca9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"></img>
-        </li>
-        <li><img src="https://images.unsplash.com/photo-1494488180300-4c634d1b2124?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"></img>
-        </li>
-        <li><img src="https://images.unsplash.com/photo-1466500419182-8602dc906b51?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
+        <li><img src="https://images.unsplash.com/photo-1517245311680-643ccb80eb87?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
         </li>
         <li><img src="https://images.unsplash.com/photo-1499462994724-775b2edaf559?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
+        </li>
+        <li><img src="https://images.unsplash.com/photo-1484371924254-a44002341ca9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"></img>
+        </li>
+        <li><img src="https://images.unsplash.com/photo-1494488180300-4c634d1b2124?ixlib=rb-1.2.1"></img>
+        </li>
+        <li><img src="https://images.unsplash.com/photo-1531268987752-b48c6b456bb8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9"></img>
+        </li>
+        <li><img src="https://images.unsplash.com/photo-1466500419182-8602dc906b51?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9"></img>
         </li>
         <li><img src="https://images.unsplash.com/photo-1518563172008-e56c5dfbaef6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
         </li>
@@ -28,15 +34,13 @@ function App() {
         </li>
         <li><img src="https://images.unsplash.com/photo-1496413651143-777c348e8329?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1086&q=80"></img>
         </li>
+        <li><img src="https://images.unsplash.com/photo-1529349462515-1def6992a0d6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
+        </li>
         <li><img src="https://images.unsplash.com/photo-1496442226666-8d4d0e62e6e9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"></img>
         </li>
         <li><img src="https://images.unsplash.com/photo-1495407123977-951ef41c11fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
         </li>
         <li><img src="https://images.unsplash.com/photo-1541475798349-e6428f2d199a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
-        </li>
-        <li><img src="https://images.unsplash.com/photo-1529349462515-1def6992a0d6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
-        </li>
-        <li><img src="https://images.unsplash.com/photo-1517245311680-643ccb80eb87?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
         </li>
         <li><img src="https://images.unsplash.com/photo-1530581453246-0fbc7bbda601?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"></img>
         </li>
