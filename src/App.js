18669da4d27ddbda7f1f7f8c9f7d022ca020a6a6
chrisclarkedev <ceecee@CeeCees-MacBook-Air.local>
2019-10-31 10:57:43 -0400

    added testing image

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
       <h1>Testing</h1>
+      <ul>
+        <li><img src="https://images.unsplash.com/photo-1491001224967-2c0ee18a7c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
+        </li>
+      </ul>
     </div>
   );
 }
